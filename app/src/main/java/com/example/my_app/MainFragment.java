bb19baeb7ca0bc53731c9fd9ede030b0eaaf137b
Suraj Arya <surajkumarsah165@gmail.com>
2023-08-25 18:20:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/my_app/MainFragment.java
@@ -0,0 +1,111 @@
+package com.example.my_app;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainFragment extends Fragment {
+
+    FirebaseDatabase database;
+    DatabaseReference mDatabase;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("category");
+        final String[] cat = {""};
+
+        mDatabase.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    cat[0] = String.valueOf(task.getResult().getValue());
+                    Log.d("firebasefdjfdjfjdkfjkdjfkdjf", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), cat[0], Toast.LENGTH_SHORT).show();
+            }
+        });
+        ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
+        pager.setAdapter(new PagerAdapter());
+
+        TextView text = (TextView) view.findViewById(R.id.text);
+
+        return view;
+    }
+
+    private class PagerAdapter extends androidx.viewpager.widget.PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 6;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            // Create some layout params
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+            // Create some text
+            TextView textView = getTextView(container.getContext());
+            textView.setText(String.valueOf(position));
+            textView.setLayoutParams(layoutParams);
+
+            RelativeLayout layout = new RelativeLayout(container.getContext());
+            layout.setBackgroundColor(ContextCompat.getColor(container.getContext(), R.color.colorPrimary));
+            layout.setLayoutParams(layoutParams);
+
+            layout.addView(textView);
+            container.addView(layout);
+            return layout;
+        }
+
+        private TextView getTextView(Context context) {
+            TextView textView = new TextView(context);
+            textView.setTextColor(Color.WHITE);
+            textView.setTextSize(30);
+            textView.setTypeface(Typeface.DEFAULT_BOLD);
+            return textView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((RelativeLayout) object);
+        }
+    }
+}
